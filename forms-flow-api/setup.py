8d786f47c12335299f3d7d24775b213445088161
abilpraju-aot <abil.raju@aot-technologies.com>
2024-06-03 19:57:14 +0530

    j

--- forms-flow-api/setup.py
+++ forms-flow-api/setup.py
@@ -1,32 +1,18 @@
+import os
 from setuptools import find_packages, setup
-
 from glob import glob
 from os.path import basename, splitext
 
-										   
-
-
 def read_requirements(filename):
-    """Get application requirements from the requirements.txt file.
-    :return: Python requirements
-    :rtype: list
-    """
     with open(filename, "r") as req:
         requirements = req.readlines()
     install_requires = [r.strip() for r in requirements if r.find("git+") != 0]
     return install_requires
 
-
 def read(filepath):
-    """Read the contents from a file.
-    :param str filepath: path to the file to be read
-    :return: file contents
-    :rtype: str
-    """
     with open(filepath, "r") as file_handle:
         content = file_handle.read()
     return content
-
 
 REQUIREMENTS = read_requirements("requirements.txt")
 
@@ -49,3 +35,9 @@ setup(
         "pytest",
     ],
 )
+
+# Debugging prints
+print("Files in the package directory:")
+for root, dirs, files in os.walk("src/formsflow_api_utils"):
+    for file in files:
+        print(os.path.join(root, file))
